a4cb37c92f815a162961551f44d8eb0cc8f466a9
cyct123 <cyct123@gmail.com>
2021-12-17 20:55:28 +0800

    feat:finish 2nd version of prompt:use editline

--- prompt.c
+++ prompt.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/readline.h>
 
 int main(int argc, char** argv) {
+	/* Print Version and Exit Information */
+  	puts("Lispy Version 0.0.0.0.1");
+  	puts("Press Ctrl+c to Exit\n");
 
-  /* Print Version and Exit Information */
-  puts("Lispy Version 0.0.0.0.1");
-  puts("Press Ctrl+c to Exit\n");
+	/* In a never ending loop */
+	while (1) {
+		
+		/* Output our prompt and get input */
+		char* input = readline("lispy> ");
 
-  /* In a never ending loop */
-  while (1) {
+	    /* Add input to history */
+	    add_history(input);
 
-    /* Output our prompt */
-    fputs("lispy> ", stdout);
+	    /* Echo input back to user */
+	    printf("No you're a %s\n", input);
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
-
-    /* Echo input back to user */
-    printf("No you're a %s", input);
-  }
-
-  return 0;
+	    /* Free retrieved input */
+	    free(input);
+		}
+	return 0;
 }
